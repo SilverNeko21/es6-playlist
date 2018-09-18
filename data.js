40bff414c6f891e2e7d6ab2d94395b4d9d5ea3c4
SilverNeko21 <larong2122@gmail.com>
2018-09-17 14:17:16 -1000

    oof

---
+++ data.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+        artist: "Bob Ross",
+        genere: "ASMR",
+        song: "Beat the devil out of it",
+        image: ""
+    },
+    {
+        artist: "The Japanese",
+        genere: "ASMR",
+        song: "Neko",
+        image: ""
+    },
+    {
+        artist: "The Japanese",
+        genere: "ASMR",
+        song: "Ear licking",
+        image: ""
+    },
+    {
+        artist: "The Japanese",
+        genere: "ASMR",
+        song: "Mushy sounds",
+        image: ""
+    },
+
+
+    {
+        artist: "Filthy Frank",
+        genere: "Rap",
+        song: "Fried Noodles",
+        image: ""
+    },
+
+
+    {
+        artist: "RADWIMPS",
+        genere: "Anime",
+        song: "Sparkle",
+        image: ""
+    },
+
+
+    {
+        artist: "Aragon",
+        genere: "Gospel",
+        song: "Hallelujah",
+        image: ""
+    },
+
+];
