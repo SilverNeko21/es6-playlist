da249c9434fb4f891bae3dfaf459e2c367000d6a
SilverNeko21 <larong2122@gmail.com>
2018-09-19 01:23:19 -1000

    Needs work

--- data.js
+++ data.js
@@ -1,38 +1,82 @@
 const data = [
+ // Dubstep   
     {
-        artist: "Bob Ross",
-        genere: "ASMR",
-        song: "Beat the devil out of it",
+        artist: "Skrillex",
+        genere: "Dubstep",
+        song: "Bangarang",
         image: ""
     },
     {
-        artist: "The Japanese",
-        genere: "ASMR",
-        song: "Neko",
+        artist: "Skrillex",
+        genere: "Dubstep",
+        song: "First Of The Year(Equinox)",
         image: ""
     },
     {
-        artist: "The Japanese",
-        genere: "ASMR",
-        song: "Ear licking",
+        artist: "Knife Party",
+        genere: "Dubstep",
+        song: "Centipede",
         image: ""
     },
     {
-        artist: "The Japanese",
-        genere: "ASMR",
-        song: "Mushy sounds",
+        artist: "Skrillex",
+        genere: "Dubstep",
+        song: "Scary Monsters And Nice Sprites",
+        image: ""
+    },
+    {
+        artist: "Meg and Dia",
+        genere: "Dubstep",
+        song: "Meg & Dia - Monster (DotEXE Dubstep Remix)",
+        image: ""
+    },
+    {
+        artist: "Skrillex",
+        genere: "Dubstep",
+        song: "KYOTO",
         image: ""
     },
 
-
+// Rap   
+    {
+        artist: "Ne3on",
+        genere: "Rap",
+        song: "Nippontradamus",
+        image: ""
+    },
     {
         artist: "Filthy Frank",
         genere: "Rap",
         song: "Fried Noodles",
         image: ""
     },
+    {
+        artist: "Filthy Frank",
+        genere: "Rap",
+        song: "Fried Rice",
+        image: ""
+    },
+    {
+        artist: "Filthy Frank",
+        genere: "Rap",
+        song: "Ramen and Rap",
+        image: ""
+    },
+    {
+        artist: "Desiigner",
+        genere: "Rap",
+        song: "Panda",
+        image: ""
+    },
+    {
+        artist: "DBamgz",
+        genere: "Rap",
+        song: "Thick Niggas and Anime Tiddies",
+        image: ""
+    },
 
 
+// Anime
     {
         artist: "RADWIMPS",
         genere: "Anime",
@@ -41,11 +85,68 @@ const data = [
     },
 
 
+// Nightcore
     {
-        artist: "Aragon",
-        genere: "Gospel",
-        song: "Hallelujah",
+        artist: "Syrex",
+        genere: "Nightcore",
+        song: "Nightcore - Ievan Polkka (VSNS Remix)",
+        image: ""
+    },
+    {
+        artist: "TheNightcoreWitcher",
+        genere: "Nightcore",
+        song: "Nightcore - Rockefeller Street",
+        image: ""
+    },
+    {
+        artist: "NightcoreReality",
+        genere: "Nightcore",
+        song: "Nightcore - Take A Hint",
+        image: ""
+    },
+    {
+        artist: "Nightcore Lab",
+        genere: "Nightcore",
+        song: "Nightcore - Die Young",
+        image: ""
+    },
+    {
+        artist: "CUTLoveRx",
+        genere: "Nightcore",
+        song: "Nightcore - Angel With A Shotgun",
+        image: ""
+    },
+    {
+        artist: "NightcoreZodiac",
+        genere: "Nightcore",
+        song: "Nightcore - Senpai",
         image: ""
     },
 
+
+// My Favorites
+    {
+        artist: "My Chemical Romance",
+        genere: "Alternative/Indie",
+        song: "Welcome To The Black Parade",
+        image: ""
+    },
+    {
+        artist: "Panic! At The Disco",
+        genere: "Nightcore",
+        song: "This Is Gospel",
+        image: "Alternative/Indie"
+    },
+    {
+        artist: "Draft Punk",
+        genere: "Trap",
+        song: "Harder, Better, Faster, Stronger (Far Out Remix)",
+        image: ""
+    },
+    {
+        artist: "Meg and Dia",
+        genere: "Dubstep/Nightcore",
+        song: "Meg & Dia - Monster (Nightcore Dubstep Remix)",
+        image: ""
+    },
 ];
